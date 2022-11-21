199d3f1f3da6cf1092accb486839d9d8a2a7f59a
Emmanuel <ejaramilo@hotmail.com>
2022-11-21 10:38:01 -0500

    323 adding first pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// our-domain.com/
+
+function HomePage() {
+  return (
+    <h1>The home page!</h1>
+  );
+}
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,9 @@
+// our-domain.com/news
+
+function NewsPage() {
+  return (
+    <h1>The news page!</h1>
+  );
+}
+
+export default NewsPage;
