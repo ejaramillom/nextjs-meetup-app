d6d19971f4667fac4f0738edb613d96a551a3758
Emmanuel <ejaramilo@hotmail.com>
2022-11-22 10:44:57 -0500

    331 adding the new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // our-domain.com/new-meetup
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupFunction() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupFunction;
