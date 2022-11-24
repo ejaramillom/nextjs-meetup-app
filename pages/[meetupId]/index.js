f97c109b8ef9baf85a8fce26c1472561d4b2779d
Emmanuel <ejaramilo@hotmail.com>
2022-11-24 10:26:52 -0500

    334 adding custom components

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,17 @@
 // our-domain.com/new-meetup-id
+
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/4/40/Panor%C3%A1mica_Oto%C3%B1o_Alc%C3%A1zar_de_Segovia.jpg"
+      title="first meetup"
+      address="Some street 5, some city"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
