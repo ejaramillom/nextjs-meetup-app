5f72c9f97327c748d5e4fb05be1e8bc262f515a1
Emmanuel <ejaramilo@hotmail.com>
2022-11-28 10:16:22 -0500

    341 introducing API routes in nextjs

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,12 @@
+// /api/new-meetup
+// POST /api/new-meetup
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+  }
+}
+
+export default handler;
