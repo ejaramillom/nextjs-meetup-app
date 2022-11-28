85f0d21688f02d36bd18e31ce7fd08fd5787c213
Emmanuel <ejaramilo@hotmail.com>
2022-11-28 10:52:15 -0500

    342 working with mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,26 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://emmjar:theinfestral28*@testcluster.nfp1n.mongodb.net/meetupsNextjs?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
